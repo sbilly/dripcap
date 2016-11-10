fce5fdac11143218eba365c73cca7e37a5f0979b
Ron Hashimoto <mail@h2so5.net>
2016-11-10 20:13:14 +0900

    refactor MACAddress parser

--- dripcap/mac.js
+++ dripcap/mac.js
@@ -1,7 +1,8 @@
 import {Value} from 'dripcap';
 
 export function MACAddress(buffer) {
-  let val = buffer.toString('hex').replace(/..(?=.)/g, '$&:');
+  let hex = buffer.toString('hex');
+  let val = `${hex[0]}${hex[1]}:${hex[2]}${hex[3]}:${hex[4]}${hex[5]}:${hex[6]}${hex[7]}:${hex[8]}${hex[9]}:${hex[10]}${hex[11]}`;
   return new Value(val, 'dripcap/mac');
 }
 
