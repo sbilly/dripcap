2716a47461a2af137757714cc8f96bf55bde71fe
Ron Hashimoto <mail@h2so5.net>
2016-11-17 20:58:39 +0900

    deploy proper rpm package; fix #9

---
+++ dripcap.rpm.spec
@@ -0,0 +1,43 @@
+%define        __spec_install_post %{nil}
+%define          debug_package %{nil}
+%define        __os_install_post %{_dbpath}/brp-compress
+
+Summary: Caffeinated packet analyzer
+Name: dripcap
+Version: {{DRIPCAP_VERSION}}
+Release: 1
+License: MIT
+Requires(post): libcap
+AutoReqProv: no
+
+%description
+Dripcap is a modern packet analyzer based on Electron.
+
+%prep
+
+%build
+
+%install
+rm -rf %{buildroot}
+mkdir -p  %{buildroot}
+cp -a * %{buildroot}
+
+%clean
+
+%files
+%defattr(-,root,root)
+%doc
+
+/usr/bin/dripcap
+/usr/share/dripcap/
+/usr/share/applications/dripcap.desktop
+/usr/share/doc/dripcap/copyright
+/usr/share/icons/hicolor/256x256/apps/dripcap.png
+/usr/share/lintian/overrides/dripcap
+
+
+%changelog
+
+%post
+setcap cap_net_raw,cap_net_admin=eip /usr/share/dripcap/dripcap
+
--- travis/linux-before-deploy.sh
+++ travis/linux-before-deploy.sh
@@ -1,11 +1,28 @@
 chmod -R g-w debian
 gulp out
 gulp debian
+rm -r .debian/usr/share/dripcap
 mv .builtapp/Dripcap-linux-x64 .debian/usr/share/dripcap
 mv .debian/usr/share/dripcap/Dripcap .debian/usr/share/dripcap/dripcap
 cd .debian
 chrpath -r /usr/share/dripcap ./usr/share/dripcap/dripcap
 fakeroot dpkg-deb --build . ../dripcap-linux-amd64.deb
 cd ..
-fakeroot alien --to-rpm -k --scripts dripcap-linux-amd64.deb
-mv *.rpm dripcap-linux-amd64.rpm
+
+# rpm
+mkdir -p ~/rpm/SPECS
+mkdir -p ~/rpm/SOURCES
+mkdir -p ~/rpm/BUILD
+mkdir -p ~/rpm/RPMS
+mkdir -p ~/rpm/SRPMS
+
+DRIPVER=$(jq -r '.version' package.json)
+sed -e "s/{{DRIPCAP_VERSION}}/$DRIPVER/g" dripcap.rpm.spec > ~/rpm/dripcap.rpm.spec
+
+cat <<EOF >~/.rpmmacros
+%_topdir   %(echo $HOME)/rpm
+EOF
+
+cp -r .debian/usr ~/rpm/BUILD
+rpmbuild -bb ~/rpm/dripcap.rpm.spec
+mv ~/rpm/RPMS/*/*.rpm dripcap-linux-amd64.rpm
