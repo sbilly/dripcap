b64df1a4aec9fa34cbccf80d75abbd61353993b6
Ron Hashimoto <mail@h2so5.net>
2016-12-20 21:25:55 +0900

    yarn

--- travis/windows-before-install.ps1
+++ travis/windows-before-install.ps1
@@ -7,6 +7,5 @@ Copy-Item -Path ../dripcap -Destination ../dripcap2 -Recurse
 
 $env:NO_WPCAP = "true"
 npm config set loglevel error
-npm install --depth 0 -g gulp electron babel-cli node-gyp
-npm install --depth 0 nan babel-plugin-add-module-exports babel-plugin-transform-es2015-modules-commonjs
-npm install --depth 0
+yarn global add gulp electron babel-cli node-gyp
+yarn
